1a091b8994fba525642b62d12a549442202126ff
jonasber06 <144693393+jonasber06@users.noreply.github.com>
2026-01-20 22:27:41 +0100

    oppdatert logikk for kollisjon. Bruker nå innebygde funksjoner i pygame-biblioteket for å sjekke kollisjon mellom rect-objektene som hver hindring består av og en usynlig 'hitbox' som følger fuglen.

--- spill.py
+++ spill.py
@@ -18,7 +18,7 @@ class App:
     def __init__(self):
         self.height = HEIGHT
         self.width = WIDTH
-        self.score = 0
+        self._score = 0
         self._high_score = 0
     
     def setup(self):
@@ -39,14 +39,16 @@ class App:
         self.hindring1.render(self.vindu)
         self.hindring2.render(self.vindu)
 
-        if self.bird.G == 0:
+        if self.bird.G == 0: #før spillet er i gang, fuglen har 0 i G
             font = pg.font.SysFont(None,17)
             text = font.render(f'Trykk på mellomrom / SPACE', True, (0, 0, 0))        
             self.vindu.blit(text, (self.width // 2 -75 , self.height // 2+50))
         
+        
+        
         font_score = pg.font.SysFont(None,30)
         hs_text = font_score.render(f'Highscore: {self._high_score}', True, (0,0,0))
-        score_text = font_score.render(f'Nåværende score: {self.score}', True, (0,0,0))
+        score_text = font_score.render(f'Nåværende score: {self._score}', True, (0,0,0))
         self.vindu.blit(hs_text,(self.width // 11 , self.height // 10))
         self.vindu.blit(score_text,(self.width // 11 , (self.height // 10)+35))
 
@@ -83,43 +85,45 @@ class App:
 
                 #poengsum oppdateres i det en av hindringene går utenfor skjermen på venstre side. Da er fuglen akkurat kommet gjennom den 'andre' hindringen, slik at poenglogikken fungerer som ønsket. 
                 if self.hindring2.x == WIDTH:
-                    self.score += 1
+                    self._score += 1
                 elif self.hindring1.x == WIDTH:
-                    self.score += 1
+                    self._score += 1
 
             #kollisjonssjekk
-            #hindring 1
-                if self.bird.x in range(int(self.hindring1.x-self.bird.r) , int(self.hindring1.x+self.hindring1.width+self.bird.r)): #dersom fuglen, med noe ekstra margin, er ved en hindring i x-retning
-                    if self.bird.y+self.bird.r >= self.hindring1.y+self.hindring1.diff: 
-                        #dersom fuglens y (pluss radius) er "større" enn det hindringens y er. (nedre del)
-                        #Dette dekker både kollisjon når fuglen er i mellom hindringene, og dersom man kolliderer utenfor hindringene
+
+                fugl_rect = self.bird.hent_rect()
+
+                #hindring 1
+                for rect_objekt in self.hindring1.hent_rect():
+                    if fugl_rect.colliderect(rect_objekt): #.colliderect() gir en boolsk verdi som gir true dersom fugl_rect og rect_objekt kolliderer
+                        self.dod()
+                
+                #hindring2
+                for rect_objekt in self.hindring2.hent_rect():
+                    if fugl_rect.colliderect(rect_objekt):
                         self.dod()
 
-                    elif self.bird.y-self.bird.r <= self.hindring1.y:   
-                        #derson fuglens y, minus radius, (fuglens ytre kant) er "mindre" enn hindringens y, har den med øvre del av hindringen
-                        self.dod()
-
-            #hindring 2, samme logikk som over
-                if self.bird.x in range(int(self.hindring2.x-self.bird.r) , int(self.hindring2.x+self.hindring2.width+self.bird.r)):
-                    if self.bird.y+self.bird.r >= self.hindring2.y+self.hindring2.diff:
-                        self.dod()
-
-                    elif self.bird.y-self.bird.r <= self.hindring2.y:
-                        self.dod()
-
-                if self.bird.y + self.bird.dy > self.height: #dersom fuglen faller ned på bunnen av skjermen er spillet også over
+                if self.bird.hent_y() + self.bird.hent_dy() > self.height: #dersom fuglen faller ned på bunnen av skjermen er spillet også over
                     self.dod()
+                
+                if self.bird.hent_y() + self.bird.hent_dy() < 0: #dersom fuglen flyr over 
+                    self.bird.sett_dy(0)
 
             clock.tick(60)
     
     def dod(self):
-        if self.score > self._high_score:
-            self._high_score = self.score
+        if self._score > self._high_score:
+            self._high_score = self._score
         
-        self.score = 0
+        self._score = 0
         self.bird.reset()
+
+        self.hindring1.nyRunde()
+        self.hindring2.nyRunde() #for å få nye høyder på hindringen til den nye runden. denne metoden flytter og x, men det tas hånd om i linjene under
+
         self.hindring1.sett_x(self.width)
         self.hindring2.sett_x(self.width * 1.5) #resetter fugl og hindringer til slik de var etter setup() metoden
+
 
 class Bird:
     def __init__(self,x,y):
@@ -129,6 +133,8 @@ class Bird:
         self.r = 15
         self._start_y = y
         self.x = x
+        self.fugl = pg.transform.scale(fugl,(6*self.r,6*self.r))
+        self.rect = pg.Rect(0,0,2*self.r,2*self.r)
 
 
     def beveg(self):
@@ -136,16 +142,15 @@ class Bird:
         self.dy = 0
         self.dy -= 7 
         
-
     def gravitasjon(self):
         self.dy += self.G
         self.y += self.dy
 
     def render(self,vindu):
-        self.fugl = pg.transform.scale(fugl,(6*self.r,6*self.r))
-        self.vinkel = 0
 
+        self.vinkel = 0
         #bruker en vinkel som fuglen får basert på hvor stor hastighet den har i y-retning
+
         if self.dy < 0:
             self.vinkel = 20
         
@@ -155,21 +160,35 @@ class Bird:
         elif self.dy > 0:
             self.vinkel = -20
 
-
         elif self.dy == 0:
             self.vinkel = 0
 
         self.rotert = pg.transform.rotate(self.fugl,self.vinkel)
         self.ny_fugl = self.rotert.get_rect(center=(self.x, self.y))
 
-        vindu.blit(self.rotert, self.ny_fugl.topleft) #.topleft fordi øverst til venstre er "sentrum" av fuglen. 
+        vindu.blit(self.rotert, self.ny_fugl)
+        #pg.draw.rect(vindu,(0,0,0),self.hent_rect(),1) #denne linjen kan kommenteres inn/ut for å vise fuglens 'hitbox'
+    
+    def hent_rect(self):
+        rect = pg.Rect(self.x,self.y - self.r, 2 * self.r, 2* self.r) #dette rect-objektet fungerer som en slags hitbox for fuglen
+        rect.centerx = WIDTH // 2
+        return rect
     
     def reset(self):
         self.G = 0
         self.dy = 0
         self.y = self._start_y
+    
+    def hent_y(self):
+        return self.y
+    
+    def hent_dy(self):
+        return self.dy
 
-class Hindring:
+    def sett_dy(self,parameter):
+        self.dy = parameter
+
+class Hindring: 
     def __init__(self,x):
         self.x = x
         self.y = r.randint(HEIGHT // 3, HEIGHT-HEIGHT//3 )
@@ -184,29 +203,34 @@ class Hindring:
         self.y = r.randint(HEIGHT // 3, HEIGHT-HEIGHT//3)
   
     def beveg(self):
-        if self.x + self.width < 0:
+        if self.x + self.width < 0: #dersom hindringen er på venstre side av skjermen
             self.nyRunde()
         else:
             self.x -= 2
     
     def sett_x(self,parameter):
         self.x = parameter
+    
+    def hent_rect(self):
+        return [
+            pg.Rect(self.x,0,self.width,self.y),
+            pg.Rect(self.x - 5,self.y - (HEIGHT // 20),self.width + 10,HEIGHT // 20),
+            pg.Rect(self.x, self.y + self.diff, self.width, HEIGHT - (self.y + self.diff)),
+            pg.Rect(self.x-5,self.y + self.diff,self.width+ 10,HEIGHT // 20)
+            ]
+        #henter rect-objekter for hindringen
+        # 0: topp, hoved
+        # 1: topp, kant
+        # 2: bunn, hoved
+        # 3: bunn, kant
 
     def render(self,vindu):
-        #øvre halvdel
-        pg.draw.rect(vindu,(0,255,0),pg.Rect(self.x,0,self.width,self.y)) #hovedtegning
-        pg.draw.rect(vindu,(0,0,0),pg.Rect(self.x,0,self.width,self.y),1) #kantlinje 
-        #kant
-        pg.draw.rect(vindu,(0,255,0),pg.Rect(self.x - 5,self.y - (HEIGHT // 20),self.width + 10,HEIGHT // 20)) #hovedtegning
-        pg.draw.rect(vindu,(0,0,0),pg.Rect(self.x - 5,self.y - (HEIGHT // 20),self.width + 10,HEIGHT // 20),1) #kantlinje
+        rects = self.hent_rect() #henter oppdaterte rect-objekter
 
+        for rect in rects: #tegner basert på de oppdaterte rect-objektene
+            pg.draw.rect(vindu,(0,255,0),rect) #grønn del
+            pg.draw.rect(vindu,(0,0,0),rect,1) #kantlinje
 
-        #nedre halvdel 
-        pg.draw.rect(vindu,(0,255,0),pg.Rect(self.x,self.y+self.diff,self.width,HEIGHT)) 
-        pg.draw.rect(vindu,(0,0,0),pg.Rect(self.x,self.y+self.diff,self.width,HEIGHT),1) 
-        #kant
-        pg.draw.rect(vindu,(0,255,0),pg.Rect(self.x-5,self.y + self.diff,self.width+ 10,HEIGHT // 20)) #hovedtegning
-        pg.draw.rect(vindu,(0,0,0),pg.Rect(self.x-5,self.y + self.diff,self.width+ 10,HEIGHT // 20),1) #kantlinje
 
 a = App()
 a.run()
